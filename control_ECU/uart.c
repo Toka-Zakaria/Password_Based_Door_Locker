48b30e6921cb55508d8b9aecddd69ecd387aa11d
Toka-Zakaria <75904835+Toka-Zakaria@users.noreply.github.com>
2021-01-20 00:34:01 +0200

    Update uart.c

--- control_ECU/uart.c
+++ control_ECU/uart.c
@@ -1,9 +1,13 @@
-/*
- * uart.c
+/**********************************************************************************
+ * [FILE NAME]: uart.c
  *
- *  Created on: Oct 1, 2020
- *      Author: user
- */
+ * [AUTHOR]: Toka Zakaria Mohamed Ramadan
+ *
+ * [DATE CREATED]: Nov 5, 2020
+ *
+ * [Description]: Function to initialize uart driver
+ *
+ ***********************************************************************************/
 
 #include "uart.h"
 
@@ -11,14 +15,30 @@
 void UART_init(const UART_config * cofig_Ptr)
 {
 
+	/************************** UCSRB Description **************************
+	 * RXCIE = 0 Disable USART RX Complete Interrupt Enable
+	 * TXCIE = 0 Disable USART Tx Complete Interrupt Enable
+	 * UDRIE = 0 Disable USART Data Register Empty Interrupt Enable
+	 * RXEN  = 1 Receiver Enable
+	 * RXEN  = 1 Transmitter Enable
+	 * UCSZ2 = 0 For 8-bit data mode
+	 * RXB8 & TXB8 not used for 8-bit data mode
+	 ***********************************************************************/
 	UCSRB |= (1 << RXEN) | (1 << TXEN);
+
+	UCSRB = (UCSRB & 0xFB) | (cofig_Ptr->data_bits & 0x04);
+
+	/************************** UCSRC Description **************************
+	 * URSEL   = 1 The URSEL must be one when writing the UCSRC
+	 * UMSEL   = 0 Asynchronous Operation
+	 * UPM1:0  = 00 Disable parity bit
+	 * USBS    = 0 One stop bit
+	 * UCSZ1:0 = 11 For 8-bit data mode
+	 * UCPOL   = 0 Used with the Synchronous operation only
+	 ***********************************************************************/
 	UCSRC |= (1 << URSEL) ;
 
-	/*double speed*/
-	UCSRA = (UCSRA & 0xFD) | (double_speed << 1);
-
 	UCSRC = (UCSRC & 0xF9) | ((cofig_Ptr->data_bits & 0x03)<<1);
-	UCSRB = (UCSRB & 0xFB) | (cofig_Ptr->data_bits & 0x04);
 
 	/*parity*/
 	UCSRC = (UCSRC & 0xCF) | (cofig_Ptr->parity << 4);
@@ -29,20 +49,33 @@ void UART_init(const UART_config * cofig_Ptr)
 	/*mode*/
 	UCSRC = (UCSRC & 0xBF) | (mode_bit << 6);
 
+	/* U2X = 1 for double transmission speed */
+	UCSRA = (UCSRA & 0xFD) | (double_speed << 1);
+
+	/* First 8 bits from the BAUD_PRESCALE inside UBRRL and last 4 bits in UBRRH*/
 	UBRRL = BAUD_PRESCALE;
+
 	UBRRH = BAUD_PRESCALE >> 8;
-}
+}/*end of Uart_init function*/
 
 
 void UART_sendByte(uint8 data)
 {
+	/* UDRE flag is set when the Tx buffer (UDR) is empty and ready for
+	 * transmitting a new byte so wait until this flag is set to one */
 	while (BIT_IS_CLEAR(UCSRA ,UDRE)){};
+	/* put the required data in the UDR register and it also clear the UDRE flag as
+	 * the UDR register is not empty now */
 	UDR = data;
 }
 
 uint8 UART_recieveByte(void)
 {
+	/* RXC flag is set when the UART receive data so wait until this
+	 * flag is set to one */
 	while (BIT_IS_CLEAR(UCSRA ,RXC)){};
+	/* Read the received data from the Rx buffer (UDR) and the RXC flag
+	   will be cleared after read this data */
 	return UDR;
 }
 
